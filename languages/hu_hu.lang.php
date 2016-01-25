6bb3c2ba35e74c7e30f7dad8ece9ce9258cb9dd7
Joe Bordes <joe@tsolucio.com>
2016-01-25 23:34:50 +0100

    i18n(hu)

---
+++ languages/hu_hu.lang.php
@@ -0,0 +1,242 @@
+<?php
+/*********************************************************************************
+** The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+*
+ ********************************************************************************/
+
+$app_strings  = Array(
+
+//General
+'hu_hu' => "Magyar",
+'customerportal' => 'Ügyfélkapu',
+'MYC Vtiger Customer Portal' => 'Ügyfélkapu belépés',
+'Login' => 'Bejelentkezés',
+'E-mail' => 'Eamil cím',
+'Password' => 'Jelszó',
+'Forgot Password' => 'Elfelejtett jelszó',
+'Request Password' => 'Jelszó kérése',
+'Send Request' => 'Kérelem elküldése',
+'Go to Login' => 'Tovább a belépéshez',
+'Home' => 'Kezdőlap',
+'No dashboard infos found!' => 'Nem találhatóak vezérlőpult infók!',
+'The record could not be found!' => 'Rekord nem található!',
+'Dashboard' => 'Vezérlőpult',
+'Logout' => 'Kijelentkezés',
+'Please Sign In' => 'Kérjük, jelentkezzen be',
+'Show Menu' => 'Menü megjelenítése',
+'Toggle navigation' => 'Menü bezárása',
+'File' => 'Fájl',
+'Upload' => 'Feltöltés',
+'Download' => 'Letöltés',
+'Attachments' => 'Mellékletek',
+'Change Password' => 'Jelszó módosítása',
+'Old Password' => 'Régi jelszó',
+'New Password' => 'Új jelszó',
+'Confirm New Password' => 'Új jelszó megerősítése',
+'Close' => 'Bezár',
+'View All' => 'Megtekintés:',
+'m/d/Y' => 'Y/m/d',
+'Monday' => 'Hétfő',
+'Tuesday' => 'Kedd',
+'Wednesday' => 'Szerda',
+'Thursday' => 'Csütörtök',
+'Friday' => 'Péntek',
+'Saturday' => 'Szombat',
+'Sunday' => 'Vasárnap',
+'UPLOAD_COMPLETED' => 'Feltöltés befejeződött.',
+'LBL_UPLOAD_VALID_FILE' => 'Kérjük, érvényes fájlt töltsön fel.',
+'LBL_ENTER_VALID_USER' => 'Kérjük, érvényes felhasználónevet és jelszót adjon meg',
+'MORE_THAN_ONE_USER' => 'Több felhasználó is létezik ezekkel az adatokkal. Kérjük, lépjen kapcsolatba az adminisztrátorral.',
+'INVALID_USERNAME_OR_PASSWORD' => 'A felhasználónév vagy a jelszó helytelen!',
+'MSG_ENTER_NEW_PASSWORDS_SAME' => 'Az új és a megerősítő jelszó nem egyezik',
+'LBL_NOTSET_UPLOAD_DIR'=>'NEM ÁLLÍTOTT BE FELTÖLTÉSI KÖNYVTÁRAT A CONFIG FÁJLBAN',
+'LBL_GIVE_VALID_FILE'=>'Kérjük, érvényes fájlt töltsön fel!',
+'LBL_UPLOAD_FILE_LARGE'=>'Sajnáljuk, de a feltöltött fájl mérete meghaladja a maximális feltöltési méretet. Kérjük, próbálja meg egy kisebb fájllal',
+'LBL_PROBLEM_UPLOAD'=>'Probléma a feltöltéssel. Kérjük, próbálja újra!',
+'LBL_FILE_HAS_NO_CONTENTS'=>'A fájlnak nincs tartalma, így nem tudja feltölteni',
+'LBL_PROBLEM_IN_TICKET_SAVING'=>'<br> A bejelentés mentése közben hiba léphetett fel. Kérjük, ellenőrizze, hogy a bejelentés elkészült-e vagy sem.',
+
+//Fresh theme
+' Home' => ' Kezdőlap',
+'  E-mail' => 'Eamil cím',
+'  Password' => 'Jelszó',
+'Sign in to your account' => 'Jelentkezzen be fiókjába',
+
+//JS strings
+'Search:' => 'Keresés:',
+'Processing...' => 'Feldolgozás',
+'Show _MENU_ entries'  => '_MENU_ bejegyzés oldalanként',
+'Showing _START_ to _END_ of _TOTAL_ entries'  => '_START_ - _END_ bejegyzés / összesen _TOTAL_',
+'Showing 0 to 0 of 0 entries'  => '0 találat',
+'(filtered from _MAX_ total entries)'  => '(_MAX_ bejegyzésbő)',
+'Loading...'  => 'Betöltés...',
+'No matching records found'  => 'Nem találhatóak egyező rekordok',
+'No data available in table'  => 'Nincsenek adatok a táblázatban',
+'Previous'  => 'Előző',
+'Next' => 'Következő',
+'First'  => 'Első',
+'Last'  => 'Utolsó',
+': activate to sort column ascending'  => ': activate to sort column ascending',
+': activate to sort column descending'  => ': activate to sort column descending',
+
+//Tab names
+'HelpDesk' => 'Bejelentések',
+'Faq' => 'GYIK',
+'Accounts' => 'Szervezet',
+'Contacts' => 'Kapcsolatok',
+'Products' => 'Termékek',
+'Services' => 'Szolgáltatások',
+'Quotes' => 'Ajánlatok',
+'Invoice' => 'Számlák',
+'Documents' => 'Dokumentumok',
+'Project' => 'Projektek',
+'ProjectTask' => 'Projektfeladatok',
+'ProjectMilestone' => 'Projekt mérföldkövek',
+'Assets' => 'Ügyletek',
+'Events' => 'Események',
+
+//Tickets
+'No HelpDesk records Found!' => 'Bejelentés nem található!',
+'New Ticket' => 'Új bejelentés',
+'Close Ticket' => 'Bejelentés lezárása',
+'Enter the title of the ticket' => 'Írja be a bejelentés nevét',
+'Send Comment' => 'Megjegyzés elküldése',
+'Submit Ticket' => 'Bejelentés elküldése',
+'Ticket Comments' => 'Megjegyzések',
+'Ticket Detail' => 'Bejelentés részletei',
+'Ticket Status' => 'Bejelentés állapota',
+'Open'  => 'Nyitott',
+'Title'  => 'Megnevezés',
+'Assigned To' => 'Felelős',
+'Priority'  => 'Prioritás',
+'Severity'  => 'Súlyosság',
+'Contact Name' => 'Kapcsolattartó',
+'Hours'  => 'Óra',
+'Days'  => 'Nap',
+'Ticket No'  => 'Bejelentés száma',
+'Related to'  => 'Érintett',
+'Product Name'  => 'Terméknév',
+'Related Product' => 'Kapcsolódó termék',
+'Status'  => 'Állapot',
+'Category'  => 'Kategória',
+'Description'  => 'Leírás',
+'Solution'  => 'Megoldás',
+'In Progress' => 'Folyamatban',
+'Wait For Response' => 'Válaszra vár',
+'Closed' => 'Lezárt' ,
+'Subject' => 'Megnevezés',
+'Contract Service' => 'Szolgáltatási szerződés',
+'Low' => 'Alacsony',
+'Normal' => 'Normál',
+'High' => 'Magas',
+'Urgent' => 'Sürgős',
+'Big Problem' => 'Nagyobb probléma',
+'Small Problem' => 'Kisebb probléma',
+'Other Problem' => 'Egyéb probléma',
+'Minor' => 'Kisebb',
+'Major' => 'Jelentős',
+'Feature' => 'Kiemelt',
+'Critical' => 'Kritikus',
+
+//Faq
+'Sorry... No F.A.Q. in this Category!' => 'Sajnáljuk... Nincs kérdés ebben a kategóriában!',
+'Categories' => 'Kategóriák',
+'Answer' => 'Válasz',
+'General' => 'Általános',
+
+//Quotes
+'No Quotes records Found!' => 'Ajánlat nem található!',
+'Created' => 'Létrehozva',
+'Delivered' => 'Teljesítve',
+'Reviewed' => 'Átvizsgálva',
+'Accepted' => 'Elfogadva',
+'Rejected' => 'Elutasítva',
+
+//Invoices
+'No Invoice records Found!' => 'Számla nem található!',
+'AutoCreated' => 'Automatikusan létrehozva',
+'Cancel' => 'Sztornó',
+'Approved' => 'Jóváhagyva',
+'Sent' => 'Elküldve',
+'Credit Invoice' => 'Előleg',
+'Paid' => 'Fizetve',
+
+//Products
+'No Products records Found!' => 'Termék nem található!',
+'Quotes Related Products' => 'Ajánlatokhoz kapcsolódó termékek',
+'Invoices Related Products' => 'Számlákhoz kapcsolódó termékek',
+'Part Number' => 'Cikkszám',
+'Commission Rate' => 'Jutalék',
+'Qty/Unit' => 'Mennyiség/db',
+'Unit Price' => 'Egységár',
+'Related To' => 'Érintett',
+
+//Services
+'No Services records Found!' => 'Szolgáltatás nem található!',
+
+//Documents
+'No Documents records Found!' => 'Dokumentum nem található!',
+
+//Contacts
+'No Contacts records Found!' => 'Kapcsolat nem található!',
+
+//Accounts
+'No Accounts records Found!' => 'Szervezet nem található!',
+
+//Project
+'No Project records Found!' => 'Projekt nem található!',
+'administrative' => 'Adminisztratív',
+'operative' => 'Operatív',
+'Other' => 'Egyéb',
+'prospecting' => 'Lehetőség',
+'initiated' => 'Kezdeményezett',
+'in progress' => 'Folyamatban',
+'waiting for feedback' => 'Visszajelzésre vár',
+'on hold' => 'Szüneteltetve',
+'completed' => 'Befejezve',
+'delivered' => 'Teljesítve',
+'archived' => 'Archiválva',
+'Project Related Tickets' => 'Projekthez kapcsolódó bejelentések',
+
+
+//ProjectTask
+'No ProjectTask records Found!' => 'Projektfeladat nem található!',
+'Project Related Tasks' => 'Projekthez kapcsolódó feladatok',
+'Project Task Name' => 'Projektfeladat megnevezés',
+'Start Date' => 'Kezdete',
+'End Date' => 'Vége',
+'Type' => 'Típus',
+'Progress' => 'Előrehaladás',
+
+//ProjectMilestone
+'No ProjectMilestone records Found!' => 'Projekt mérföldkő nem található!',
+'Project Related Milestones' => 'Projekthez kapcsolódó mérföldkövek',
+'Project Milestone Name' => 'Projekt mérföldkő megnevezés',
+'Milestone Date' => 'Mérföldkő dátum',
+
+//Assets
+'No Assets records Found!' => 'Ügylet nem található!',
+
+//Events
+'No Events records Found!' => 'Esemény nem található!',
+'Events Calendar' => 'Eseménynaptár',
+'Calendar Mode' => 'Naptár',
+'List Mode' => 'Lista',
+'today' => 'ma',
+'month' => 'hónap',
+'week' => 'hét',
+'day' => 'nap',
+'SUN' => 'V',
+'MON' => 'H',
+'TUE' => 'K',
+'WED' => 'Sze',
+'THU' => 'Cs',
+'FRI' => 'P',
+'SAT' => 'Szo',
+);
+?>
