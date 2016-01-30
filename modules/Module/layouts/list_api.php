5fe6fdd3cbd60c25019a9ad5b0d4355c184e0f7b
Omar Llorens <omarllorens@gmail.com>
2016-01-31 00:13:47 +0100

    fix(List Layout) module label is not translating

--- modules/Module/layouts/list_api.php
+++ modules/Module/layouts/list_api.php
@@ -26,7 +26,7 @@
                 <div class="col-lg-12">
                     <div class="panel panel-default">
                         <div class="panel-heading">
-                            <?php echo $module; ?>
+                            <?php echo Language::translate($module); ?>
                         </div>
                         <!-- /.panel-heading -->
                         <div class="panel-body text-center">
